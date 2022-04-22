1279cc9d1990e0c4cf50ffd96370830e23378649
Gerard Paligot <gerard.paligot@gmail.com>
2022-04-22 16:01:04 +0200

    chore: Update Compose to 1.2.0-alpha07.

--- ui/src/main/java/com/paligot/conferences/ui/components/events/PartnerItem.kt
+++ ui/src/main/java/com/paligot/conferences/ui/components/events/PartnerItem.kt
@@ -1,14 +1,12 @@
 package com.paligot.conferences.ui.components.events
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.Indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -17,7 +15,6 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.ImageLoader
@@ -36,7 +33,6 @@ fun PartnerItem(
     backgroundColor: Color = MaterialTheme.colors.surface,
     shape: Shape = MaterialTheme.shapes.medium,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    indication: Indication = rememberRipple(),
     contentScale: ContentScale = ContentScale.Fit,
     onClick: (siteUrl: String?) -> Unit
 ) {
@@ -46,13 +42,11 @@ fun PartnerItem(
         }
         .build()
     Surface(
-        modifier = modifier.placeholder(visible = isLoading),
-        role = Role.Button,
-        indication = indication,
-        interactionSource = interactionSource,
-        color = backgroundColor,
-        shape = shape,
         onClick = { onClick(partnerUi.siteUrl) },
+        modifier = modifier.placeholder(visible = isLoading),
+        shape = shape,
+        color = backgroundColor,
+        interactionSource = interactionSource
     ) {
         Image(
             painter = rememberAsyncImagePainter(
@@ -73,8 +67,7 @@ fun PartnerItemPreview() {
     Conferences4HallTheme {
         PartnerItem(
             partnerUi = PartnerItemUi.fake,
-            modifier = Modifier.size(200.dp),
-            onClick = {}
-        )
+            modifier = Modifier.size(200.dp)
+        ) {}
     }
 }
