15eaa69c4402af2716a19c5791b0c6a2dbe1c71a
Gerard Paligot <gerard.paligot@gmail.com>
2022-04-30 22:43:51 +0200

    chore(androidApp): Create release 0.1.1.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,7 +11,7 @@ val accompanistVersion: String by project
 val settingsVersion: String by project
 val versionMajor = 0
 val versionMinor = 1
-val versionPatch = 0
+val versionPatch = 1
 android {
     compileSdk = 31
     defaultConfig {
