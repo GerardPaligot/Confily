d8c63cc3a0e5048eaf31c9b81e3fdc3757b8f55b
Gerard Paligot <gerard.paligot@gmail.com>
2022-04-30 21:57:08 +0200

    refactor: Fetch network data and collect agenda in parallel.

--- androidApp/src/main/java/org/gdglille/devfest/android/screens/agenda/AgendaViewModel.kt
+++ androidApp/src/main/java/org/gdglille/devfest/android/screens/agenda/AgendaViewModel.kt
@@ -3,11 +3,13 @@ package org.gdglille.devfest.android.screens.agenda
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import org.gdglille.devfest.repositories.AgendaRepository
-import org.gdglille.devfest.models.AgendaUi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import org.gdglille.devfest.models.AgendaUi
+import org.gdglille.devfest.repositories.AgendaRepository
 
 sealed class AgendaUiState {
     data class Loading(val agenda: AgendaUi) : AgendaUiState()
@@ -23,17 +25,22 @@ class AgendaViewModel(
 
     init {
         viewModelScope.launch {
-            try {
-                repository.fetchAndStoreAgenda()
-            } catch (ignored: Throwable) {
-            }
-            try {
-                repository.agenda().collect {
-                    _uiState.value = AgendaUiState.Success(it)
+            arrayListOf(async {
+                try {
+                    repository.fetchAndStoreAgenda()
+                } catch (ignored: Throwable) {
                 }
-            } catch (ignore: Throwable) {
-                _uiState.value = AgendaUiState.Failure(ignore)
-            }
+            }, async {
+                try {
+                    repository.agenda().collect {
+                        if (it.talks.isNotEmpty()) {
+                            _uiState.value = AgendaUiState.Success(it)
+                        }
+                    }
+                } catch (ignore: Throwable) {
+                    _uiState.value = AgendaUiState.Failure(ignore)
+                }
+            }).awaitAll()
         }
     }
 
