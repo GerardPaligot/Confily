d82cfcd04d921884124a95ec2babd9d9ee11b82a
Gerard Paligot <gerard.paligot@gmail.com>
2022-05-01 14:02:01 +0200

    chore: Update zxing to 3.4.1.

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -109,5 +109,5 @@ dependencies {
     implementation("com.journeyapps:zxing-android-embedded:4.3.0") {
         isTransitive = false
     }
-    implementation("com.google.zxing:core:3.3.0")
+    implementation("com.google.zxing:core:3.4.1")
 }
