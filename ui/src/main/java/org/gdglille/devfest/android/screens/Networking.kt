e697a16b25f6de921539d7c4a37446537e9cb222
Gerard Paligot <gerard.paligot@gmail.com>
2022-05-05 23:23:42 +0200

    feat(android): Add a message when there is no networking profile.

--- ui/src/main/java/org/gdglille/devfest/android/screens/Networking.kt
+++ ui/src/main/java/org/gdglille/devfest/android/screens/Networking.kt
@@ -3,7 +3,11 @@ package org.gdglille.devfest.android.screens
 import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.AlertDialog
@@ -14,8 +18,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.gdglille.devfest.android.components.users.UserItem
@@ -30,50 +36,70 @@ fun Networking(
     modifier: Modifier = Modifier,
     onNetworkDeleted: (String) -> Unit
 ) {
-    val openDialog = remember { mutableStateOf(false) }
-    LazyColumn(
-        modifier = modifier,
-        contentPadding = PaddingValues(vertical = 4.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        itemsIndexed(users) { index, user ->
-            UserItem(
-                user = user,
-                modifier = Modifier.clickable { openDialog.value = true }
-            )
-            if (index < users.size - 1) {
-                Divider()
-            }
-            if (openDialog.value) {
-                AlertDialog(
-                    onDismissRequest = { openDialog.value = false },
-                    confirmButton = {
-                        Button(
-                            onClick = {
-                                onNetworkDeleted(user.email)
-                                openDialog.value = false
-                            }
-                        ) {
-                            Text(stringResource(R.string.action_submit_accept))
-                        }
-                    },
-                    dismissButton = {
-                        Button(onClick = { openDialog.value = false }) {
-                            Text(stringResource(R.string.action_submit_deny))
-                        }
-                    },
-                    text = {
-                        Text(
-                            text =
-                            stringResource(
-                                R.string.text_networking_ask_to_delete,
-                                "${user.firstName} ${user.lastName}"
-                            )
-                        )
-                    }
+    if (users.isEmpty()) {
+        EmptyNetworking(modifier = modifier)
+    } else {
+        val openDialog = remember { mutableStateOf(false) }
+        LazyColumn(
+            modifier = modifier,
+            contentPadding = PaddingValues(vertical = 4.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            itemsIndexed(users) { index, user ->
+                UserItem(
+                    user = user,
+                    modifier = Modifier.clickable { openDialog.value = true }
                 )
+                if (index < users.size - 1) {
+                    Divider()
+                }
+                if (openDialog.value) {
+                    AlertDialog(
+                        onDismissRequest = { openDialog.value = false },
+                        confirmButton = {
+                            Button(
+                                onClick = {
+                                    onNetworkDeleted(user.email)
+                                    openDialog.value = false
+                                }
+                            ) {
+                                Text(stringResource(R.string.action_submit_accept))
+                            }
+                        },
+                        dismissButton = {
+                            Button(onClick = { openDialog.value = false }) {
+                                Text(stringResource(R.string.action_submit_deny))
+                            }
+                        },
+                        text = {
+                            Text(
+                                text =
+                                stringResource(
+                                    R.string.text_networking_ask_to_delete,
+                                    "${user.firstName} ${user.lastName}"
+                                )
+                            )
+                        }
+                    )
+                }
             }
         }
+    }
+}
+
+@Composable
+fun EmptyNetworking(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(R.string.text_networking_empty),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            textAlign = TextAlign.Center
+        )
     }
 }
 
