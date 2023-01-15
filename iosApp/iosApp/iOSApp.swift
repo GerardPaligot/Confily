eec5a4852081d9377c758fa28269d5cf17edf5b5
Gerard Paligot <gerard.paligot@gmail.com>
2023-01-15 22:36:17 +0100

    fix(ios): Remove eventId from dao constructors.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -7,7 +7,6 @@ import SDWebImageSVGCoder
 @main
 struct iOSApp: App {
     let baseUrl = "https://cms4partners-ce427.nw.r.appspot.com"
-    let eventId = "2022"
     let db = DatabaseWrapper().createDb()
     
     init() {
@@ -18,20 +17,21 @@ struct iOSApp: App {
     }
 
 	var body: some Scene {
-        let api = ConferenceApi.companion.create(baseUrl: self.baseUrl, eventId: self.eventId, enableNetworkLogs: isInDebugMode)
+        let api = ConferenceApi.companion.create(baseUrl: self.baseUrl, enableNetworkLogs: isInDebugMode)
         let settings = AppleSettings(delegate: UserDefaults.standard)
         let agendaRepository = AgendaRepositoryImpl(
             api: api,
-            scheduleDao: ScheduleDao(db: db, settings: settings, eventId: eventId),
-            speakerDao: SpeakerDao(db: db, eventId: eventId),
+            scheduleDao: ScheduleDao(db: db, settings: settings),
+            speakerDao: SpeakerDao(db: db),
             talkDao: TalkDao(db: db),
-            eventDao: EventDao(db: db, eventId: eventId),
-            partnerDao: PartnerDao(db: db, eventId: eventId),
-            featuresDao: FeaturesActivatedDao(db: db, eventId: eventId),
+            eventDao: EventDao(db: db, settings: settings),
+            partnerDao: PartnerDao(db: db),
+            featuresDao: FeaturesActivatedDao(db: db),
             qrCodeGenerator: QrCodeGeneratoriOS()
         )
         let userRepository = UserRepositoryImpl(
-            userDao: UserDao(db: db, eventId: eventId),
+            userDao: UserDao(db: db),
+            eventDao: EventDao(db: db, settings: settings),
             qrCodeGenerator: QrCodeGeneratoriOS()
         )
 		WindowGroup {
