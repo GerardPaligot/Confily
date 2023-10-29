c4b25cbbd476faed9a0706e049793a14e97afd47
Gerard Paligot <gerard.paligot@gmail.com>
2023-10-29 13:41:40 +0100

    feat(style-theme): add custom preview annotations.

---
+++ theme-m3/style/theme/src/main/kotlin/org/gdglille/devfest/android/theme/m3/style/previews/ThemedPreviews.kt
@@ -0,0 +1,8 @@
+package org.gdglille.devfest.android.theme.m3.style.previews
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+annotation class ThemedPreviews()
