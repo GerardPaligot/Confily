152f9781cfea1df897fa4fab3881d97df9ba7f69
Gerard Paligot <gerard.paligot@gmail.com>
2023-11-09 00:12:10 +0100

    fix(partners-feature): remove unused import.

--- theme-m3/partners/partners-feature/src/main/kotlin/org/gdglille/devfest/android/theme/m3/partners/feature/PartnersVM.kt
+++ theme-m3/partners/partners-feature/src/main/kotlin/org/gdglille/devfest/android/theme/m3/partners/feature/PartnersVM.kt
@@ -1,6 +1,5 @@
 package org.gdglille.devfest.android.theme.m3.partners.feature
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
