73caf4b475fc02af5860b48024876ad4028b84cf
Gerard Paligot <gerard.paligot@gmail.com>
2023-12-22 20:38:25 +0100

    refactor(ios): remove exceptions in RemoteImage.

--- iosApp/iosApp/components/structures/RemoteImage.swift
+++ iosApp/iosApp/components/structures/RemoteImage.swift
@@ -14,39 +14,23 @@ struct RemoteImage: View {
     let url: String
     let description: String?
     let id: String?
-    private let isException: Bool
-    // Need to be removed after the Devfest Lille event
-    private let exceptions: [String: String] = [
-        "hw0KkSyFq1FaTh8hz57C": "adeo-leroymerlin",
-        "K8aq8RSjQy8vqHAd6uHZ": "azfalte",
-        "PHwo6wyY149snPikjCxA": "cgi",
-        "C6cfJWrlr1XZHit4eMPK": "elosi"
-    ]
     
     init(url: String, description: String?, id: String? = nil) {
         self.url = url
         self.description = description
         self.id = id
-        self.isException = id != nil && exceptions.keys.contains(id!)
     }
 
     var body: some View {
-        Group {
-            if (self.isException) {
-                Image(exceptions[id!]!)
-                    .resizable()
-            } else {
-                WebImage(url: URL(string: url)!)
-                    .placeholder(content: {
-                        Text("...")
-                    })
-                    .resizable()
+        WebImage(url: URL(string: url)!)
+            .placeholder(content: {
+                Text("...")
+            })
+            .resizable()
+            .scaledToFit()
+            .if(description != nil) { view in
+                view.accessibilityLabel(description!)
             }
-        }
-        .scaledToFit()
-        .if(description != nil) { view in
-            view.accessibilityLabel(description!)
-        }
     }
 }
 
