b61e5c90e279e2f12725f8dc919103d3d08afd4b
GerardPaligot <gerard.paligot@gmail.com>
2024-05-03 10:08:40 +0200

    fix(backend): generate twitter and linkedin url for partners from cms.

--- backend/src/main/java/org/gdglille/devfest/backend/third/parties/cms4partners/Cms4PartnersMappers.kt
+++ backend/src/main/java/org/gdglille/devfest/backend/third/parties/cms4partners/Cms4PartnersMappers.kt
@@ -1,22 +1,45 @@
 package org.gdglille.devfest.backend.third.parties.cms4partners
 
 import org.gdglille.devfest.models.inputs.PartnerInput
+import org.gdglille.devfest.backend.third.parties.cms4partners.PartnerInput as CmsPartnerInput
 
 fun WebhookInput.mapToPartnerInput(): PartnerInput {
-    val hasTwitter = data.twitterAccount == "" || data.twitterAccount != null
-    val hasLinkedIn = data.linkedinAccount == "" || data.linkedinAccount != null
     val hasSchemaSiteUrl = data.siteUrl?.startsWith("https://") ?: false
     return PartnerInput(
         name = data.name,
         description = data.description ?: "",
         logoUrl = data.logoUrl!!,
         siteUrl = if (hasSchemaSiteUrl) data.siteUrl!! else "https://${data.siteUrl}",
-        twitterUrl = if (hasTwitter) data.twitterAccount else null,
+        twitterUrl = data.getTwitterUrl(),
         twitterMessage = data.twitter ?: "",
-        linkedinUrl = if (hasLinkedIn) data.linkedinAccount else null,
+        linkedinUrl = data.getLinkedInUrl(),
         linkedinMessage = data.linkedin ?: "",
         address = data.address ?: "",
         sponsorings = listOf(data.sponsoring!!),
         wldId = data.wldId
     )
+}
+
+fun CmsPartnerInput.getTwitterUrl(): String? {
+    return if (twitterAccount != null && twitterAccount != "") {
+        if (twitterAccount.contains("twitter.com").not()) {
+            "https://twitter.com/${twitterAccount}"
+        } else {
+            twitterAccount
+        }
+    } else {
+        null
+    }
+}
+
+fun CmsPartnerInput.getLinkedInUrl(): String? {
+    return if (linkedinAccount != null && linkedinAccount != "") {
+        if (linkedinAccount.contains("linkedin.com").not()) {
+            "https://linkedin.com/company/${linkedinAccount}"
+        } else {
+            linkedinAccount
+        }
+    } else {
+        null
+    }
 }
