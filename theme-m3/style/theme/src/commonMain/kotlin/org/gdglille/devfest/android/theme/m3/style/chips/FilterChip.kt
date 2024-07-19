9d406dae62d125d5355180433e377a5544adb219
GerardPaligot <gerard.paligot@gmail.com>
2024-07-19 09:30:29 +0200

    feat(style/theme): add wasm target.

--- theme-m3/style/theme/src/commonMain/kotlin/org/gdglille/devfest/android/theme/m3/style/chips/FilterChip.kt
+++ theme-m3/style/theme/src/commonMain/kotlin/org/gdglille/devfest/android/theme/m3/style/chips/FilterChip.kt
@@ -1,19 +1,14 @@
 package org.gdglille.devfest.android.theme.m3.style.chips
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.tooling.preview.Preview
-import org.gdglille.devfest.android.theme.m3.style.Conferences4HallTheme
 
 @Composable
 fun FilterChip(
@@ -49,29 +44,4 @@ fun FilterChip(
         modifier = modifier,
         leadingIcon = leadingIcon
     )
-}
-
-@Preview
-@Composable
-private fun FilterChipPreview2() {
-    Conferences4HallTheme {
-        Surface {
-            Column {
-                FilterChip(
-                    label = "Mobile",
-                    imageVector = Icons.Default.Add,
-                    selected = false,
-                    onClick = {
-                    }
-                )
-                FilterChip(
-                    label = "Mobile",
-                    imageVector = Icons.Default.Add,
-                    selected = false,
-                    onClick = {
-                    }
-                )
-            }
-        }
-    }
 }
