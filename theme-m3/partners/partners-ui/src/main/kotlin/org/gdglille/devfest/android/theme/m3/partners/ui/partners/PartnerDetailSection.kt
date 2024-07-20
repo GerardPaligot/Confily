42d6fd54498358de1ce3d0d6f0410c40054f64a9
GerardPaligot <gerard.paligot@gmail.com>
2024-07-20 20:50:42 +0200

    refactor(style/components/markdown): migrate module to kmp.

--- theme-m3/partners/partners-ui/src/main/kotlin/org/gdglille/devfest/android/theme/m3/partners/ui/partners/PartnerDetailSection.kt
+++ theme-m3/partners/partners-ui/src/main/kotlin/org/gdglille/devfest/android/theme/m3/partners/ui/partners/PartnerDetailSection.kt
@@ -13,7 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.gdglille.devfest.android.theme.m3.style.Conferences4HallTheme
 import org.gdglille.devfest.android.theme.m3.style.events.socials.SocialsSection
-import org.gdglille.devfest.android.theme.m3.style.markdown.MarkdownText
+import org.gdglille.devfest.theme.m3.style.markdown.MarkdownText
 import org.gdglille.devfest.android.theme.m3.style.partners.items.PartnerItem
 import org.gdglille.devfest.theme.m3.style.placeholder.placeholder
 import org.gdglille.devfest.models.ui.PartnerItemUi
