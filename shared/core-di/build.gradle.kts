bc0ab186209801c49a2e70f4fc1ca276dd3f99d6
GerardPaligot <gerard.paligot@gmail.com>
2024-08-31 08:35:11 +0200

    chore: use android-emulator-runner instead of gradle managed devices.

--- shared/core-di/build.gradle.kts
+++ shared/core-di/build.gradle.kts
@@ -50,12 +50,10 @@ kotlin {
             }
         }
         if (OperatingSystem.current().isMacOsX) {
-            val iosX64Main by getting
             val iosArm64Main by getting
             val iosSimulatorArm64Main by getting
             val iosMain by creating {
                 dependsOn(commonMain)
-                iosX64Main.dependsOn(this)
                 iosArm64Main.dependsOn(this)
                 iosSimulatorArm64Main.dependsOn(this)
             }
--- shared/core/build.gradle.kts
+++ shared/core/build.gradle.kts
@@ -87,12 +87,10 @@ kotlin {
             }
         }
         if (OperatingSystem.current().isMacOsX) {
-            val iosX64Main by getting
             val iosArm64Main by getting
             val iosSimulatorArm64Main by getting
             val iosMain by creating {
                 dependsOn(commonMain)
-                iosX64Main.dependsOn(this)
                 iosArm64Main.dependsOn(this)
                 iosSimulatorArm64Main.dependsOn(this)
                 dependencies {
@@ -100,12 +98,10 @@ kotlin {
                     implementation(libs.cash.sqldelight.native)
                 }
             }
-            val iosX64Test by getting
             val iosArm64Test by getting
             val iosSimulatorArm64Test by getting
             val iosTest by creating {
                 dependsOn(commonTest)
-                iosX64Test.dependsOn(this)
                 iosArm64Test.dependsOn(this)
                 iosSimulatorArm64Test.dependsOn(this)
             }
