d102aeabe144a0a441692d4a3597335e7458517f
GerardPaligot <gerard.paligot@gmail.com>
2024-08-31 08:35:11 +0200

    chore: use android-emulator-runner instead of gradle managed devices.

--- shared/models/build.gradle.kts
+++ shared/models/build.gradle.kts
@@ -36,12 +36,10 @@ kotlin {
         val androidMain by getting
         val jvmMain by getting
         if (OperatingSystem.current().isMacOsX) {
-            val iosX64Main by getting
             val iosArm64Main by getting
             val iosSimulatorArm64Main by getting
             val iosMain by creating {
                 dependsOn(commonMain)
-                iosX64Main.dependsOn(this)
                 iosArm64Main.dependsOn(this)
                 iosSimulatorArm64Main.dependsOn(this)
             }
