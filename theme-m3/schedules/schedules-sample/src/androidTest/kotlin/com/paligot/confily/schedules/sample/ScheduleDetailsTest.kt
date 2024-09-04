77ccbeedacf6c047825a84185b75219b2624fd61
GerardPaligot <gerard.paligot@gmail.com>
2024-09-04 22:29:06 +0200

    refactor(schedules-sample): move source files to new package names.

--- theme-m3/schedules/schedules-sample/src/androidTest/kotlin/com/paligot/confily/schedules/sample/ScheduleDetailsTest.kt
+++ theme-m3/schedules/schedules-sample/src/androidTest/kotlin/com/paligot/confily/schedules/sample/ScheduleDetailsTest.kt
@@ -1,4 +1,4 @@
-package org.gdglille.devfest.android.theme.m3.schedules.sample
+package com.paligot.confily.schedules.sample
 
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.core.app.ActivityScenario
@@ -10,15 +10,15 @@ import com.paligot.confily.core.test.instrumentedModule
 import com.paligot.confily.core.test.patterns.navigation.RobotNavigator
 import com.paligot.confily.core.test.patterns.navigation.robotHost
 import com.paligot.confily.schedules.di.scheduleModule
+import com.paligot.confily.schedules.sample.fakes.AgendaFake.agenda
+import com.paligot.confily.schedules.sample.fakes.AgendaFake.category
+import com.paligot.confily.schedules.sample.fakes.AgendaFake.format
+import com.paligot.confily.schedules.sample.fakes.AgendaFake.schedule
+import com.paligot.confily.schedules.sample.fakes.AgendaFake.session
+import com.paligot.confily.schedules.sample.fakes.AgendaFake.speaker
+import com.paligot.confily.schedules.sample.fakes.EventFake.event
 import com.paligot.confily.schedules.test.robot.schedules
 import com.paligot.confily.schedules.test.scheduleRobotGraph
-import org.gdglille.devfest.android.theme.m3.schedules.sample.fakes.AgendaFake.agenda
-import org.gdglille.devfest.android.theme.m3.schedules.sample.fakes.AgendaFake.category
-import org.gdglille.devfest.android.theme.m3.schedules.sample.fakes.AgendaFake.format
-import org.gdglille.devfest.android.theme.m3.schedules.sample.fakes.AgendaFake.schedule
-import org.gdglille.devfest.android.theme.m3.schedules.sample.fakes.AgendaFake.session
-import org.gdglille.devfest.android.theme.m3.schedules.sample.fakes.AgendaFake.speaker
-import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -27,7 +27,7 @@ import org.koin.test.KoinTest
 import org.koin.test.KoinTestRule
 import org.koin.test.inject
 
-class FilteringScheduleTest : KoinTest {
+class ScheduleDetailsTest : KoinTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
@@ -43,11 +43,12 @@ class FilteringScheduleTest : KoinTest {
     @Before
     fun setup() {
         eventDao.insertEventId(BuildConfig.DEFAULT_EVENT)
+        eventDao.insertEvent(event, emptyList())
         scheduleDao.saveAgenda(BuildConfig.DEFAULT_EVENT, agenda)
     }
 
     @Test
-    fun shouldDisplayOnlyFavoriteSessionItem() {
+    fun shouldOpenScheduleAndDisplaySessionAndSpeakerInfo() {
         ActivityScenario.launch(MainActivity::class.java).use {
             val navigator = RobotNavigator()
             robotHost(navigator) {
@@ -55,28 +56,17 @@ class FilteringScheduleTest : KoinTest {
             }
             schedules(navigator) {
                 assertSessionIsShown(show = true, session.title, listOf(speaker.displayName), schedule.room, format.time, category.name)
-            } goToFilterScreen {
-                assertFilterScreenIsOpened()
-                selectOnlyFavorites()
+            } goToSecheduleDetail {
+                assertSessionDetails(
+                    session.title,
+                    session.abstract,
+                    listOf(speaker.displayName),
+                    schedule.room,
+                    format.time,
+                    category.name
+                )
             } backToScheduleGrid {
-                assertSessionIsShown(show = false, session.title, listOf(speaker.displayName), schedule.room, format.time, category.name)
-            } goToFilterScreen {
-                assertFilterScreenIsOpened()
-                selectOnlyFavorites()
-            } backToScheduleGrid {
-                assertSessionIsShown(show = true, session.title, listOf(speaker.displayName), schedule.room, format.time, category.name)
-                addSessionToFavorites(session.title, listOf(speaker.displayName), schedule.room, format.time, category.name)
-            } goToFilterScreen {
-                assertFilterScreenIsOpened()
-                selectOnlyFavorites()
-            } backToScheduleGrid {
-                assertSessionIsShown(show = true, session.title, listOf(speaker.displayName), schedule.room, format.time, category.name)
             }
         }
-    }
-
-    @After
-    fun tearDown() {
-        scheduleDao.applyFavoriteFilter(false)
     }
 }
--- theme-m3/schedules/schedules-sample/src/main/kotlin/com/paligot/confily/schedules/sample/MainApplication.kt
+++ theme-m3/schedules/schedules-sample/src/main/kotlin/com/paligot/confily/schedules/sample/MainApplication.kt
@@ -1,4 +1,4 @@
-package org.gdglille.devfest.android.theme.m3.schedules.sample
+package com.paligot.confily.schedules.sample
 
 import com.paligot.confily.core.sample.SampleApplication
 import com.paligot.confily.core.sample.buildConfigModule
