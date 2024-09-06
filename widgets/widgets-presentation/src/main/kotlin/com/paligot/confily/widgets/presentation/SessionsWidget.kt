4df4708e09e0631aab0daf2853c4c9dbc34f5a0a
GerardPaligot <gerard.paligot@gmail.com>
2024-09-06 21:33:10 +0200

    refactor(widgets-presentation): move source files to new package names.

--- widgets/widgets-presentation/src/main/kotlin/com/paligot/confily/widgets/presentation/SessionsWidget.kt
+++ widgets/widgets-presentation/src/main/kotlin/com/paligot/confily/widgets/presentation/SessionsWidget.kt
@@ -1,4 +1,4 @@
-package org.gdglille.devfest.android.widgets.feature
+package com.paligot.confily.widgets.presentation
 
 import androidx.annotation.DrawableRes
 import androidx.compose.runtime.Composable
