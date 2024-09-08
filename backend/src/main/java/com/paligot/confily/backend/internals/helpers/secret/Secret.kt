adaf6d0bf26b9402d16d25fee26acf3e2461d144
GerardPaligot <gerard.paligot@gmail.com>
2024-09-08 13:10:52 +0200

    refactor(backend): secret values can be nullable.

--- backend/src/main/java/com/paligot/confily/backend/internals/helpers/secret/Secret.kt
+++ backend/src/main/java/com/paligot/confily/backend/internals/helpers/secret/Secret.kt
@@ -3,7 +3,7 @@ package com.paligot.confily.backend.internals.helpers.secret
 import com.google.cloud.secretmanager.v1.SecretManagerServiceClient
 
 interface Secret {
-    operator fun get(id: String): String
+    operator fun get(id: String): String?
 
     object Factory {
         fun create(projectId: String, client: SecretManagerServiceClient): Secret =
