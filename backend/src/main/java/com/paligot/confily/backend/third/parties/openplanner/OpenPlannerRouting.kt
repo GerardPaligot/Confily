1b2fbb5cbedcab5c65a964388cdcafb1fb62366a
GerardPaligot <gerard.paligot@gmail.com>
2024-12-07 22:38:54 +0100

    refactor(backend): generate new agenda version automatically with OP.

--- backend/src/main/java/com/paligot/confily/backend/third/parties/openplanner/OpenPlannerRouting.kt
+++ backend/src/main/java/com/paligot/confily/backend/third/parties/openplanner/OpenPlannerRouting.kt
@@ -1,6 +1,7 @@
 package com.paligot.confily.backend.third.parties.openplanner
 
 import com.paligot.confily.backend.NotAuthorized
+import com.paligot.confily.backend.events.EventModule.eventRepositoryV4
 import com.paligot.confily.backend.third.parties.openplanner.OpenPlannerModule.openPlannerRepository
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.response.respond
@@ -9,10 +10,12 @@ import io.ktor.server.routing.post
 
 fun Routing.registerOpenPlannerRoutes() {
     val repository by openPlannerRepository
+    val eventRepository by eventRepositoryV4
 
     post("openplanner/webhook") {
         val eventId = call.parameters["eventId"]!!
         val apiKey = call.request.queryParameters["api_key"] ?: throw NotAuthorized
-        call.respond(HttpStatusCode.Created, repository.update(eventId, apiKey))
+        repository.update(eventId, apiKey)
+        call.respond(HttpStatusCode.Created, eventRepository.generateAgenda(eventId, apiKey))
     }
 }
