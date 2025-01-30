db7f965782aec17e15fc451da1f4dfacaae24d67
GerardPaligot <gerard.paligot@gmail.com>
2025-01-30 20:21:44 +0100

    feat(style): add jvm target.

---
+++ style/components/adaptive/src/desktopMain/kotlin/com/paligot/confily/style/components/adaptive/BackHandler.desktop.kt
@@ -0,0 +1,8 @@
+package com.paligot.confily.style.components.adaptive
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun BackHandler(enabled: Boolean, onBack: () -> Unit) {
+    /* no-op */
+}
